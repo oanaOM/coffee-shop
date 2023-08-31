28688cd3cdc9caaf8b04aca59089bf610d630f06
Oana Munteanu <hello2oana@yahoo.com>
2023-08-31 10:44:21 +0100

    feat: naive API to play with goroutines

---
+++ README.md
@@ -0,0 +1,64 @@
+# GopherCon UK -2023
+
+## Scaling Coffee with Goroutines
+
+Build a small web app that serves coffees during the workshop held by Sadie Freeman @sadief.
+
+### Requirements
+
+- it has to be scalable and fast!
+
+### Implementation steps:
+
+- Server coffee without concurrency
+- Server more than 1 coffee
+- Add concurrency: use goroutines
+- Add WaitGroup:
+  Why? to make sure that all goroutines we start will also finish
+- Add more goroutines: so we can run each function concurrent
+
+### How to run it?
+
+- in root folder run `make run` to start the docker container
+- navigate to `http://localhost:8080/serve-coffee/50000` to hit the endpoint and try making MANY coffee. Checkout the logs to see the time it took to make them. In my case, 50000 coffee took 13.390567749s
+- in root folder run `make down` to stop the docker container and remove image.
+
+### What's next?
+
+- Scale vertically by adding more pods
+
+### Testing Results
+
+Configuration 1
+
+```
+resources:
+            limits:
+              cpu: 1
+              memory: 3092M
+            requests:
+              cpu: 1
+              memory: 1024M
+```
+
+// Took 5.057038683s to serve coffee, customer no: 50000
+
+Configuration 2
+
+```
+          resources:
+            limits:
+              cpu: 4
+              memory: 3092M
+            requests:
+              cpu: 2
+              memory: 1024M
+```
+
+Took 9.362377223s to serve coffee, customer no: 50000
+
+Why? 🤔
+
+##### Resources
+
+https://github.com/sadief/gophercon-2023-slides-code/tree/main
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	portNo := ":8080"
+	http.HandleFunc("/serve-coffee/", ServeCoffee)
+
+	log.Printf("Coffee server starting on: %s", portNo)
+	http.ListenAndServe(portNo, nil)
+}
+
+// ServeCoffee is setup of the coffee shop API
+func ServeCoffee(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	// pass to the URL the number of customers
+	noCustomers, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/serve-coffee/"))
+
+	// Q: is there another way to initialise the noCustomers
+	if err != nil || noCustomers == 0 {
+		noCustomers = 1
+	}
+
+	// handler to wait for multiple goroutines to finish https://gobyexample.com/waitgroups
+	wg := sync.WaitGroup{}
+	// counter to keep track of customers
+	count := 0
+	for i := 0; i < noCustomers; i++ {
+		wg.Add(1)
+		go MakeCoffee(&wg)
+		count++
+	}
+	wg.Wait()
+
+	timeTaken := time.Since(start)
+	log.Printf("Took %s to serve coffee, customer no: %v", timeTaken, count)
+}
+
+// MakeCoffee is responsible to the process of serving a coffee
+func MakeCoffee(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// add more goroutines for each action in particular
+	newWg := sync.WaitGroup{}
+	newWg.Add(3)
+
+	go TakePayment(&newWg)
+	go MakeEspresso(&newWg)
+	go FoamMilk(&newWg)
+	newWg.Wait()
+}
+
+// MakeEspresso is responsible to make the espresso
+func MakeEspresso(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(2 * time.Second)
+	log.Printf("Coffee: Espresso is done.")
+}
+
+// FoamMilk is responsible to foam the milk
+func FoamMilk(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	time.Sleep(2 * time.Second)
+	log.Printf("Milk: Milk is ready.")
+
+}
+
+// TakePayment is responsible to take the payment from the customer
+func TakePayment(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	time.Sleep(2 * time.Second)
+	log.Printf("Money: Payment has been taken")
+}
